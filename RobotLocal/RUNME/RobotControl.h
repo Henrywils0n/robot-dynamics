82af7b3e7f4e32cc2c82305341b50820996e8d7e
John Doe <keegkelly2013@gmail.com>
2022-06-01 20:45:26 -0400

    made some stuff private in the control class

--- RobotLocal/RUNME/RobotControl.h
+++ RobotLocal/RUNME/RobotControl.h
@@ -28,33 +28,25 @@ void clearRightEncoder()
 {
     rightEncoderTicks = 0;
 }
+// same as fix theta but it changes a given angle instead of the robot heading
+float fixAngle(float angle)
+{
+    while (angle > pi)
+    {
+        angle -= 2 * pi;
+    }
+    while (angle < -pi)
+    {
+        angle += 2 * pi;
+    }
+    return angle;
+}
 // Robot class for controlling the robot
 class Robot
 {
 public:
-    // radius of the wheels
-    float r = 0.034;
-    // radius from the centre to the wheel
-    float R = 0.082;
     // position of the robot on the grid
     float x, y, theta;
-    // direction of each wheel, 1 is forward, 0 is backward
-    byte DirWL, DirWR;
-    // encoder ticks for each wheel at last update (used for calculating distance)
-    int prevLeftEncoderTicks = 0;
-    int prevRightEncoderTicks = 0;
-    float prevMagTheta;
-    // Linear velocity gains
-    float Kp = 1.5;
-    float Ki = 0.005;
-    float Kd = 0.5;
-    // Angular velocity gains
-    float KpTheta = 11;
-    float KiTheta = 1;
-    float KdTheta = 1.5;
-    // Acceptable position error
-    float Err = 0.03;
-
     // sets up required pin modes and objects
     Robot(float X, float Y, float THETA)
     {
@@ -69,60 +61,6 @@ public:
         pinMode(rightEncoder, INPUT_PULLUP);
         attachInterrupt(digitalPinToInterrupt(leftEncoder), incrementLeftEncoder, CHANGE);
         attachInterrupt(digitalPinToInterrupt(rightEncoder), incrementRightEncoder, CHANGE);
-    }
-
-    // moves the robot at velocity v and angular velocity w
-    void drive(float v, float w)
-    {
-        // angular velocities
-        float wR = (v + R * w) / r;
-        float wL = (v - R * w) / r;
-        // set Directions according to the speed
-        // 1 moves forward, 0 moves backward (if this isn't true on a robot flip the wires going to the motor)
-        DirWL = (wL >= 0);
-        DirWR = (wR >= 0);
-        // using the calibrated values convert the speed to the correct PWM values
-        // robot starts moving at 103 (on laplace)
-        // at max reading W of each wheel is about 19.5 on the floor(precision does not really matter its just helpful)
-        // set the max left wheel speed a little higher to account for differences in the motors to make it drive straighter
-        int WR = map(abs(wR), 0, 19.5, 76, 255);
-        int WL = map(abs(wL), 0, 19.8, 76, 255);
-        // setting cutoffs for the motors
-        if (WR > 255)
-            WR = 255;
-        else if (abs(wR) <= 0.25)
-            WR = 0;
-        if (WL > 255)
-            WL = 255;
-        else if (abs(wL) <= 0.25)
-            WL = 0;
-        // sending signal to the motors
-        digitalWrite(DIRR, DirWR);
-        digitalWrite(DIRL, DirWL);
-        analogWrite(PWMR, WR);
-        analogWrite(PWML, WL);
-    }
-    // updates the position estimate of the robot based on the encoders
-    void updatePosition()
-    {
-        // encoder ticks since last update
-        int diffLeft = leftEncoderTicks;
-        clearLeftEncoder();
-        int diffRight = rightEncoderTicks;
-        clearRightEncoder();
-        // angle change since last update
-        float dTheta = (diffRight * (-1 + 2 * DirWR) - diffLeft * (-1 + 2 * DirWL)) * pi / 384 * r / R;
-        // distance traveled forward since last update assuming the angle traveled is constant at the previous angle plus half the angle change
-        float EncoderdL = (diffLeft * (-1 + 2 * DirWL) + diffRight * (-1 + 2 * DirWR)) * pi / 384 * r;
-        // adding a little bit of magnetometer angle for some stability it seems to be pretty accurate but not linear around a rotation
-        
-        // calculating the new position
-        float EncoderdX = EncoderdL * cos(theta + 0.5 * dTheta);
-        float EncoderdY = EncoderdL * sin(theta + 0.5 * dTheta);
-        theta += (dTheta);
-        fixTheta();
-        x += EncoderdX;
-        y += EncoderdY;
     }
     // send x,y position and the robot will move to that position
     void moveTo(float X, float Y)
@@ -191,6 +129,84 @@ public:
         // set the speed to zero once the new position is reached
         drive(0, 0);
     }
+
+    // moves the robot at velocity v and angular velocity w
+    void drive(float v, float w)
+    {
+        // angular velocities
+        float wR = (v + R * w) / r;
+        float wL = (v - R * w) / r;
+        // set Directions according to the speed
+        // 1 moves forward, 0 moves backward (if this isn't true on a robot flip the wires going to the motor)
+        DirWL = (wL >= 0);
+        DirWR = (wR >= 0);
+        // using the calibrated values convert the speed to the correct PWM values
+        // robot starts moving at 103 (on laplace)
+        // at max reading W of each wheel is about 19.5 on the floor(precision does not really matter its just helpful)
+        // set the max left wheel speed a little higher to account for differences in the motors to make it drive straighter
+        int WR = map(abs(wR), 0, 19.5, 76, 255);
+        int WL = map(abs(wL), 0, 19.8, 76, 255);
+        // setting cutoffs for the motors
+        if (WR > 255)
+            WR = 255;
+        else if (abs(wR) <= 0.25)
+            WR = 0;
+        if (WL > 255)
+            WL = 255;
+        else if (abs(wL) <= 0.25)
+            WL = 0;
+        // sending signal to the motors
+        digitalWrite(DIRR, DirWR);
+        digitalWrite(DIRL, DirWL);
+        analogWrite(PWMR, WR);
+        analogWrite(PWML, WL);
+    }
+
+private:
+    // radius of the wheels
+    float r = 0.034;
+    // radius from the centre to the wheel
+    float R = 0.082;
+    // direction of each wheel, 1 is forward, 0 is backward
+    byte DirWL, DirWR;
+    // encoder ticks for each wheel at last update (used for calculating distance)
+    int prevLeftEncoderTicks = 0;
+    int prevRightEncoderTicks = 0;
+    float prevMagTheta;
+    // Linear velocity gains
+    float Kp = 1.5;
+    float Ki = 0.005;
+    float Kd = 0.5;
+    // Angular velocity gains
+    float KpTheta = 11;
+    float KiTheta = 1;
+    float KdTheta = 1.5;
+    // Acceptable position error
+    float Err = 0.03;
+
+    // updates the position estimate of the robot based on the encoders
+    void updatePosition()
+    {
+        // encoder ticks since last update
+        int diffLeft = leftEncoderTicks;
+        clearLeftEncoder();
+        int diffRight = rightEncoderTicks;
+        clearRightEncoder();
+        // angle change since last update
+        float dTheta = (diffRight * (-1 + 2 * DirWR) - diffLeft * (-1 + 2 * DirWL)) * pi / 384 * r / R;
+        // distance traveled forward since last update assuming the angle traveled is constant at the previous angle plus half the angle change
+        float EncoderdL = (diffLeft * (-1 + 2 * DirWL) + diffRight * (-1 + 2 * DirWR)) * pi / 384 * r;
+        // adding a little bit of magnetometer angle for some stability it seems to be pretty accurate but not linear around a rotation
+
+        // calculating the new position
+        float EncoderdX = EncoderdL * cos(theta + 0.5 * dTheta);
+        float EncoderdY = EncoderdL * sin(theta + 0.5 * dTheta);
+        theta += (dTheta);
+        fixTheta();
+        x += EncoderdX;
+        y += EncoderdY;
+    }
+
     // returns theta to the bounds +/-pi
     void fixTheta()
     {
@@ -203,21 +219,9 @@ public:
             theta += 2 * pi;
         }
     }
-    // same as fix theta but it changes a given angle instead of the robot heading
-    float fixAngle(float angle)
-    {
-        while (angle > pi)
-        {
-            angle -= 2 * pi;
-        }
-        while (angle < -pi)
-        {
-            angle += 2 * pi;
-        }
-        return angle;
-    }
+
     // sets up the motor controller
-    void setupArdumoto()
+    void setupArdumoto(
     {
         // All pins should be setup as outputs:
         pinMode(PWML, OUTPUT);
