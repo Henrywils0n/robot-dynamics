9e35944ba2b068be29b1a660728a88785c7d8656
John Doe <keegkelly2013@gmail.com>
2022-06-08 15:13:31 -0400

    added fps

--- hostSystem/test.py
+++ hostSystem/test.py
@@ -1,16 +1,25 @@
 import cv2
-import numpy as np
 import time
 from arucoFind import Tracker
-frame = cv2.imread("hostSystem/all4.jpg")
-startTime = time.perf_counter()
+# open the camera
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 tracker = Tracker(marker_width=0.114, aruco_type="DICT_4X4_1000")
-endTime = time.perf_counter()
-print(endTime-startTime)
-fps = 1/(endTime-startTime)
-print(fps)
-rederedFrame = tracker.find_markerPos(frame)
-# show the frame
-cv2.imshow("Frame", rederedFrame)
-cv2.waitKey(0)
+# read the cap frame by frame and track then display the processed frame
+prevFrameTime = time.process_time()
+while True:
+    ret, frame = cap.read()
+    if ret:
+        rederedFrame = tracker.find_markerPos(frame)
+        newFrameTime = time.process_time()
+        fps = 1 / (newFrameTime - prevFrameTime)
+        # add frame rate to the rendered frame
+        cv2.putText(rederedFrame, "FPS: " + format(fps, '.2f'), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow("frame", rederedFrame)
+        prevFrameTime = newFrameTime
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# release the camera
+cap.release()
 cv2.destroyAllWindows()
