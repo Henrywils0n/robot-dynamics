a4ffa4e901a4525e64e5cb97b6d36cf759d87405
John Doe <keegkelly2013@gmail.com>
2022-06-08 16:03:26 -0400

    added focus

--- hostSystem/test.py
+++ hostSystem/test.py
@@ -4,6 +4,8 @@ from arucoFind import Tracker
 # open the camera
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+focus = 0  # min: 0, max: 255, increment:5
+cap.set(28, focus)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 tracker = Tracker(marker_width=0.114, aruco_type="DICT_4X4_1000")
