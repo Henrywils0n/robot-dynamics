15164256dcc7ecba5fabba27af5eb4bb76a0f513
Keegan Kelly <keegkelly2013@gmail.com>
2022-06-13 13:09:55 -0400

    tried imutils

--- hostSystem/PositionTracking.py
+++ hostSystem/PositionTracking.py
@@ -2,7 +2,8 @@ import cv2
 from tracker import Tracker
 import requests
 import pandas as pd
-from imutils import WebcamVideoStream
+from imutils.video import WebcamVideoStream
+import time
 targets = False
 filename = 'testData.xlsx'
 address = 'http://192.168.0.181:3000/'
@@ -13,20 +14,23 @@ if targets:
         data = {'id': i, 't': df['t'].to_list(), 'x': df['x'+str(i)].to_list(), 'y': df['y'+str(i)].to_list()}
         r = requests.put(address + 'targets/' + str(i), data=data)
 # open the camera and sets the resolution, frame rate, and focus
-vs = WebcamVideoStream(src=0).start()
-vs.stream.set(cv2.CAP_PROP_FPS, 30)
+vs = WebcamVideoStream(src=0)
 vs.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 vs.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 # lower focus focuses further away from the camera
 focus = 0  # min: 0, max: 255, increment:5
 vs.stream.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 vs.stream.set(28, focus)
+time.sleep(5)
+vs.start()
+
 # declares the aruco tracker
 tracker = Tracker(marker_width=0.114, aruco_type="DICT_4X4_1000")
 # reads the cap frame by frame and track then display the processed frame
 makeframe = True
 while True:
-    ret, frame = vs.read()
+    ret = vs.grabbed
+    frame = vs.frame
     if ret:
         rederedFrame = tracker.find_markerPos(frame, makeframe)
         for i in range(1, 4):
@@ -39,5 +43,6 @@ while True:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 # release the camera
+vs.stop()
 vs.stream.release()
 cv2.destroyAllWindows()
