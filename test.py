a86715492895ce154d05313fecd1ae02e61e24f9
John Doe <keegkelly2013@gmail.com>
2022-08-12 18:37:09 -0400

    updated documentation and removed old files

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from sympy import re
-
-address = 'http://127.0.0.1:5000/agents/1'
-
-req = requests.get(address)
-print(req.text)
-
-req = requests.put(address, json={'id': 1, 'position': [1, 2, 3]})
